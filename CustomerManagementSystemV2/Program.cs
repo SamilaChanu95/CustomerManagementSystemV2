b07cb1cb2226a785d4ead1c2ce419379475cee0d
Chanuka Samila <chanukasamila@gmail.com>
2023-10-09 20:12:03 +0530

    Add project files.

---
+++ CustomerManagementSystemV2/Program.cs
@@ -0,0 +1,32 @@
+using CustomerManagementSystemV2.Models;
+using CustomerManagementSystemV2.Services;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapCustomerEndpoints();
+
+app.Run(); 
---
+++ CustomerManagementSystemV2/Services/CustomerService.cs
@@ -0,0 +1,24 @@
+﻿using CustomerManagementSystemV2.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace CustomerManagementSystemV2.Services
+{
+    public  static class CustomerService
+    {
+        public static void MapCustomerEndpoints(this IEndpointRouteBuilder builder) 
+        {
+            builder.MapGet("customers", async (IConfiguration configuration) =>
+            {
+                var connectionString = configuration.GetConnectionString("DBConnection");
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    const string sql = "SELECT Id, FirstName, LastName, Email, DateOfBirth FROM Customer";
+                    var customers = await connection.QueryAsync<Customer>(sql);
+                    return Results.Ok(customers);
+                }
+            });
+        }
+    }
+}
