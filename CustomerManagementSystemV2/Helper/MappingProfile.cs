17a4af1291e717a804252cc2657b49f6bcd517c1
Chanuka Samila <chanukasamila@gmail.com>
2023-10-09 21:55:32 +0530

    Added AutoMapper to mapping the dtos with Models. Added new APIs for get all data & insert fuctions.

---
+++ CustomerManagementSystemV2/Helper/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CustomerManagementSystemV2.Dtos;
+using CustomerManagementSystemV2.Models;
+
+namespace CustomerManagementSystemV2.Helper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<CustomerDto, Customer>();
+            CreateMap<Customer, CustomerDto>();
+        }
+
+    }
+}
--- CustomerManagementSystemV2/Services/CustomerService.cs
+++ CustomerManagementSystemV2/Services/CustomerService.cs
@@ -1,7 +1,10 @@
-﻿using CustomerManagementSystemV2.Models;
+﻿using AutoMapper;
+using CustomerManagementSystemV2.Dtos;
+using CustomerManagementSystemV2.Models;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using System.Data;
 
 namespace CustomerManagementSystemV2.Services
 {
@@ -9,13 +12,40 @@ namespace CustomerManagementSystemV2.Services
     {
         public static void MapCustomerEndpoints(this IEndpointRouteBuilder builder) 
         {
-            builder.MapGet("customers", async (IConfiguration configuration) =>
+            builder.MapGet("/get-customers-list", async (IConfiguration configuration) =>
             {
                 var connectionString = configuration.GetConnectionString("DBConnection");
                 using (var connection = new SqlConnection(connectionString))
                 {
                     const string sql = "SELECT Id, FirstName, LastName, Email, DateOfBirth FROM Customer";
                     var customers = await connection.QueryAsync<Customer>(sql);
+                    return Results.Ok(customers);
+                }
+            });
+
+            builder.MapGet("/get-customer/{id}", async (int id, IConfiguration configuration) =>
+            {
+                var connectionString = configuration.GetConnectionString("DBConnection");
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    string sql = $@" SELECT Id, FirstName, LastName, Email, DateOfBirth FROM Customer WHERE Id = '{id}'";
+                    var customers = await connection.QueryAsync<Customer>(sql);
+                    return Results.Ok(customers);
+                }
+            });
+
+            builder.MapPost("/create-customer", async (CustomerDto customerDto, IConfiguration configuration) =>
+            {
+                var connectionString = configuration.GetConnectionString("DBConnection");
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    string sql = @" INSERT INTO Customer(FirstName, LastName, Email, DateOfBirth) VALUES (@FirstName, @LastName, @Email, @DateOfBirth)";
+                    var parameters = new DynamicParameters();
+                    parameters.Add("FirstName", customerDto.FirstName, DbType.String);
+                    parameters.Add("LastName", customerDto.LastName, DbType.String);
+                    parameters.Add("Email", customerDto.Email, DbType.String);
+                    parameters.Add("DateOfBirth", customerDto.DateOfBirth, DbType.DateTime);
+                    var customers = await connection.ExecuteAsync(sql, parameters);
                     return Results.Ok(customers);
                 }
             });
